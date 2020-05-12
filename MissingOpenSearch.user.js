681cc84f19a64ef6fafc300d45b0ed7bc07c578e
KokaKiwi <kokakiwi+git@kokakiwi.net>
2020-05-12 14:06:55 +0200

    feat: enchance UserScript and OpenSearch docs

--- MissingOpenSearch.user.js
+++ MissingOpenSearch.user.js
@@ -1,24 +1,37 @@
 // ==UserScript==
 // @name        Add missing OpenSearch
-// @namespace   https://github.com/KokaKiwi
+// @namespace   https://github.com/opensearch-database/opensearch-database
 // @version     0.1.0
 // @author      KokaKiwi
 // @license     BSD-3-Clause
-// @include     /^https://*.githubusercontent.com/*$/
+// @homepageURL https://github.com/opensearch-database/opensearch-database
+// @downloadURL https://github.com/opensearch-database/opensearch-database/raw/master/MissingOpenSearch.user.js
+// @supportURL  https://github.com/opensearch-database/opensearch-database/issues
+// @inject-into content
 // @run-at      document-start
-// @connect     githubusercontent.com
+// @noframes
 // @connect     raw.githubusercontent.com
 // ==/UserScript==
 
-(async () => {
-  if (!document.querySelector('link[rel="search"]')) {
-    const hostname = location.hostname.split('.').splice(-2).join('.');
+(function() {
+  const fetchMetadatas = async (hostname) => {
+    const metadata_url = `https://raw.githubusercontent.com/opensearch-database/opensearch-database/master/data/${hostname}/metadata.json`;
 
+    const result = await fetch(metadata_url);
+    return result.json();
+  };
+
+  const makeLink = (metadata) => {
     const link = document.createElement('link');
     link.rel = 'search';
     link.type = 'application/opensearchdescription+xml';
-    link.href = `https://raw.githubusercontent.com/opensearch-database/opensearch-database/master/data/${hostname}.xml`;
-    link.title = hostname;
+    link.href = `https://raw.githubusercontent.com/opensearch-database/opensearch-database/master/data/${hostname}/${metadata.id}.xml`;
+    link.title = metadata.title;
+
     document.head.appendChild(link);
-  }
+  };
+
+  const hostname = location.hostname.split('.').splice(-2).join('.');
+
+  fetchMetadatas(hostname).then(metadatas => metadatas.forEach(makeLink));
 })();
